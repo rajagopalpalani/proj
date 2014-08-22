0cee829eceaf6e01fce67f502cf6e50aee51dea1
Palani <rpalanideveloper@gmail.com>
2014-08-23 00:50:19 +0530

    Update admin.php
    Foundation CSS used for styles

--- admin.php
+++ admin.php
@@ -1,6 +1,8 @@
 <html>
 <head>
 <title>Admin Portal</title>
+<link rel="stylesheet" type="text/css"  href="css/foundation.css" />
+<link rel="stylesheet" type="text/css"  href="css/foundation.min.css" />
 <style>
 .height{
 min-height:400px;
@@ -9,11 +11,24 @@ min-height:400px;
 </head>
 <body>
 <?php include('config.php'); ?>
-<div class="admin-home height">
+<div class="row">
+<div class="admin-home large-12 columns">
+<h1>Admin Panel</h1>
+<div class="search large-4 columns">
+<input type="search" name="search" id="search">
 </div>
-<div class="add height">
+<div class="search large-4 columns">
+<input type="submit" name="search-submit" value="Search" id="search-submit">
+</div>
+<div class="row">
+<div class="large-12 columns" id="show-details"></div>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="add large-12 columns" style="display:none;">
 <h1>Add</h1>
-<div class="addform">
+<div class="addform large-4 columns">
 <?php
 if($_POST['add-submit']){
 $productname = $_POST['productname'];
@@ -22,15 +37,15 @@ $productdate = $_POST['productdate'];
 $productimage = file_get_contents($_FILES['productimage']['tmp_name']);
 $productimage = mysql_real_escape_string($productimage);
 $sql="Insert into project (productname,productcity,productimage,productdate) VALUES ('$productname','$productcity','$productimage','$productdate')";
-$addproduct = mysql_query($sql,$link);
+$addproduct = mysql_query($sql,$con);
 if($addproduct){
 echo "<script>alert('Added Successfully');</script>";
 }else{
 echo "<script>alert('Failed to Added');</script>";
 }
-mysql_close($link);
+mysql_close($con);
 }
- ?>
+?>
 <form action="admin.php" method="post" enctype="multipart/form-data">
 <p>Product Name : <input name="productname" id="productname" type="text"/></p>
 <p>Product City : <input name="productcity" id="productcity" type="text"/></p>
@@ -40,7 +55,9 @@ mysql_close($link);
 </form>
 </div>
 </div>
-<div class="modify height">
+</div>
+<div class="row">
+<div class="modify large-12 columns"  style="display:none;">
 <h1>Edit Details</h1>
 <div class="editform">
 <?php
@@ -59,7 +76,7 @@ echo "<script>alert('Failed to Added');</script>";
 }
 mysql_close($link);
 }
- ?>
+?>
 <form action="admin.php" method="post" enctype="multipart/form-data">
 <p>Product Name : <input name="productname" id="productname" type="text" value=""/></p>
 <p>Product City : <input name="productcity" id="productcity" type="text" value=""/></p>
@@ -68,8 +85,11 @@ mysql_close($link);
 <input type="submit" name="edit-submit" value="Update" />
 </form>
 </div>
-
 </div>
+</div>
+<div class="row">
 <div class="view height"></div>
+</div>
 </body>
 </html>
+
